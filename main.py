e214e9af16df25e7f4072af47679712c6eee6506
Kaosi Anikwe <anikwehenryasa@gmail.com>
2023-06-02 16:38:39 +0000

    minor fixes

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import folium
 import traceback
 import pandas as pd
 from datetime import datetime
 from flask import Flask, render_template, jsonify, request, url_for
-import os
-import folium
 
 app = Flask(__name__)
 app.secret_key = "secret_key"
@@ -121,61 +120,4 @@ def dynamic_plot():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-
-# timestamps = list(set(data["created"]))
-
-# formatted_time = datetime.strptime(str(timestamps[0]), "%Y-%m-%d %H:%M:%S.%f")
-
-# from_time = datetime(
-#     2021,
-#     1,
-#     4,
-# ).strftime("%Y-%m-%d %H:%M:%S.%f")
-# to_time = datetime(2021, 12, 1).strftime("%Y-%m-%d %H:%M:%S.%f")
-
-# Apply filters
-# static
-# filtered_data = data[
-#     (data["device_id"] == 164223045)
-#     & (data["created"] >= from_time)
-#     & (data["created"] <= to_time)
-# ]
-# # dynamic
-# filtered_data = data[
-#     (data["device_id"] == 164223045)
-#     & (data["created"] >= from_time)
-# ]
-
-# len(filtered_data)
-
-# # # Extract latitude and longitude columns
-# latitude = filtered_data["latitude"]
-# longitude = filtered_data["longitude"]
-
-# # Create list of tuples
-# coordinates = list(zip(latitude, longitude))
-
-# import folium
-
-# # Create a map centered at the first coordinate
-# m = folium.Map(location=[coordinates[0][0], coordinates[0][1]], zoom_start=30)
-
-# folium.PolyLine(locations=coordinates, color="blue", weight=0.5, dash_array="2").add_to(
-#     m
-# )
-
-# # Iterate over coordinates and add markers and lines
-# for i in range(len(coordinates)):
-#     # Add marker for each coordinate
-#     folium.Marker(location=[coordinates[i][0], coordinates[i][1]]).add_to(m)
-
-#     # Add line connecting consecutive coordinates
-#     if i > 0:
-#         folium.PolyLine(
-#             locations=[coordinates[i - 1], coordinates[i]], color="blue"
-#         ).add_to(m)
-
-#     # Save the map as HTML file for visualization
-# m.save("coordinates.html")
+    app.run(debug=True, port=4000, host="0.0.0.0")
